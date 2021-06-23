71a7e4fde50ddf69421f385fc86b0b3acf6add5f
mohitsoni7 <mohit.ece.ecb@gmail.com>
2021-06-23 19:42:17 +0530

    Router and lock changes
    1. Fix the package versions and update the lock file
    2. Add switch, router and withRouter in project and link with various Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+
+const HatsPage = (history) => {
+  console.log(history);
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+
+
+const JacketsPage = (history) => {
+  console.log(history);
+  return (
+    <div>
+      <h1>Jackets Page</h1>
+    </div>
+  )
+}
+
 
 function App() {
   return (
-  <div>
-    <HomePage/>
-  </div>
+    <div>
+      <Switch>
+        {/* <HomePage/> */}
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop/hats' component={HatsPage} />
+        <Route exact path='/shop/jackets' component={JacketsPage} />
+      </Switch>
+    </div>
   );
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
-        <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
-        <div className='content'>
-            <h1 className='title'>{ title.toUpperCase() }</h1>
-            <span className='subtitle'>SHOP NOW</span>
-        </div>
-    </div>
-)
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
+	var html =
+		<div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+			<div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
+			<div className='content'>
+				<h1 className='title'>{title.toUpperCase()}</h1>
+				<span className='subtitle'>SHOP NOW</span>
+			</div>
+		</div>
+	return html;
+}
 
-export default MenuItem;
+export default withRouter(MenuItem);
